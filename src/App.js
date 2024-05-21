1e47bc5bdae3622123fd89efcb00cdd7e36645a9
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2024-05-21 16:42:29 +0300

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Awesome-ass-website
-      </header>
+        <Home/>
     </div>
   );
 }
