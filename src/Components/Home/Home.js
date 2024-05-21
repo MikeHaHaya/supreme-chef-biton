304141b841f07768df3c260c32534e2aee891a8c
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2024-05-21 16:43:43 +0300

    initial structure

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Navbar from "../Navbar/Navbar";
+
+
+const Home = () => (
+  <div className="home-container">
+      <Navbar/>
+  </div>
+);
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
