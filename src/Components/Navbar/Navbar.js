0865c55d7dcaddee7066690a8d91f70d55a142e9
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2024-05-23 00:12:11 +0300

    Assets and references

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import Logo from ".../Assets/db_logo.jpg";
+import {BsCart2} from "react-icons/bs";
+import {HiOutlineBars3} from "react-icons/hi2";
+import {Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import {CommentRoundedIcon} from "@mui/icons-material/CommentRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
 
 
-const Navbar = () => (
-  <div>
-    Navbar Component
-  </div>
-);
+const Navbar = () => {
 
-Navbar.propTypes = {};
+    const [openMenu, setOpenMenu] = useState(false);
 
-Navbar.defaultProps = {};
+
+    return (
+        <div>
+        Navbar Component
+    </div>
+    )
+};
+
 
 export default Navbar;
