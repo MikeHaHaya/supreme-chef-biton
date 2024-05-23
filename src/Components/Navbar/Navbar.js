c3dbee871a8fe975f25e5fc8ffe44a1851604252
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2024-05-23 03:30:04 +0300

    Assets and references

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,23 +1,54 @@
 import React, {useState} from 'react';
-import Logo from ".../Assets/db_logo.jpg";
+import Logo from "././Assets/db_logo.jpg"; //TODO -- FIX THIS
 import {BsCart2} from "react-icons/bs";
 import {HiOutlineBars3} from "react-icons/hi2";
 import {Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
-import {CommentRoundedIcon} from "@mui/icons-material/CommentRounded";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
 
 
 const Navbar = () => {
 
     const [openMenu, setOpenMenu] = useState(false);
+    const menuOptions = [
+        {
+            text: "Home",
+            icon: <HomeIcon/>
+        },
+        {
+            text: "About",
+            icon: <InfoIcon/>
+        },
+        {
+            text: "Reviews",
+            icon: <CommentRoundedIcon/>
+        },
+        {
+            text: "Contact",
+            icon: <PhoneRoundedIcon/>
+        },
+
+    ]
 
 
     return (
-        <div>
-        Navbar Component
-    </div>
+        <nav>
+            <div className="nav-logo-container">
+                <img src={Logo} alt=""/>
+            </div>
+            <div className="navbar-links-container">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Reviews</a>
+                <a href="">Contact</a>
+                <button className="primary-button">Bookings Now</button>
+
+            </div>
+
+    </nav>
     )
 };
 
