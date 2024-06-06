aea80c970c5362e66518458ea2f1e01f64cd5200
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2024-06-07 00:48:54 +0300

    Logo size fixed, Navbar structure.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Logo from "../../Assets/db_logo.jpg";
 import {BsCart2} from "react-icons/bs";
 import {HiOutlineBars3} from "react-icons/hi2";
-import {Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
@@ -45,10 +45,30 @@ const Navbar = () => {
                 <a href="">Reviews</a>
                 <a href="">Contact</a>
                 <button className="primary-button">Bookings Now</button>
-
             </div>
 
-    </nav>
+            {/*Mobile Menu*/}
+            <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+                <Box
+                    sx={{width: 250}}
+                    role="presentation"
+                    onClick={() => setOpenMenu(false)}
+                    onKeyDown={() => setOpenMenu(false)}
+                >
+                    <List>
+                        {menuOptions.map((item) => (
+                            <ListItem key={item.text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>{item.icon}</ListItemIcon>
+                                    <ListItemText primary={item.text}/>
+                                </ListItemButton>
+                            </ListItem>
+                            ))}
+                    </List>
+
+                </Box>
+            </Drawer>
+        </nav>
     )
 };
 
